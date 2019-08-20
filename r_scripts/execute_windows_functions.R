a42a3da77b9bc9af9c2652492310d5ebeee3687b
Justin Herman <justinherman42@gmail.com>
2019-08-20 15:16:22 -0400

    changed rscripts dir

--- r_scripts/execute_windows_functions.R
+++ r_scripts/execute_windows_functions.R
@@ -1,23 +1,8 @@
 library(devtools)
-# source_github <- function(u) {
-#     # load package
-#     require(RCurl)
-#     
-#     # read script lines from website
-#     script <- getURL(u, ssl.verifypeer = FALSE)
-#     
-#     # parase lines and evaluate in the global environment
-#     eval(parse(text = script))
-# }
-# 
 
 ## Grab windows functions
-source_url("https://raw.githubusercontent.com/justinherman42/Company_project/master/build_database.R")
-source_url("https://raw.githubusercontent.com/justinherman42/Company_project/master/Windows_functions.R")
-
-
-# source_github("https://raw.githubusercontent.com/justinherman42/Company_project/master/build_database.R")
-# source_github("https://raw.githubusercontent.com/justinherman42/Company_project/master/Windows_functions.R")
+source_url("https://raw.githubusercontent.com/justinherman42/Company_project/master/r_scripts/build_database.R")
+source_url("https://raw.githubusercontent.com/justinherman42/Company_project/master/r_scripts/Windows_functions.R")
 
 ### Execute yty_Growth with Revenue 
 yty_growth("Revenue","financials")
