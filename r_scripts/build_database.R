ceb0c7ea393087864c21cfee578e591de204d167
Justin Herman <justinherman42@gmail.com>
2019-08-20 15:14:48 -0400

    changed directory for rscripts

--- r_scripts/build_database.R
+++ r_scripts/build_database.R
@@ -1,27 +1,9 @@
 library(devtools)
 
-# source_github <- function(u) {
-#     # load package
-#     require(RCurl)
-#     
-#     # read script lines from website
-#     script <- getURL(u, ssl.verifypeer = FALSE)
-#     
-#     # parase lines and evaluate in the global environment
-#     eval(parse(text = script))
-# }
 
 ## Function to grab R files from github
-source_url("https://raw.githubusercontent.com/justinherman42/Company_project/master/load_packages.R")
-source_url("https://raw.githubusercontent.com/justinherman42/Company_project/master/load_functions.R")
-
-#source_github("https://raw.githubusercontent.com/justinherman42/Company_project/master/load_packages.R")
-#source_github("https://raw.githubusercontent.com/justinherman42/Company_project/master/load_functions.R")
-
-
-#source("load_packages.R")
-#source('load_functions.R')
-
+source_url("https://raw.githubusercontent.com/justinherman42/Company_project/master/r_scripts/load_packages.R")
+source_url("https://raw.githubusercontent.com/justinherman42/Company_project/master/r_scripts/load_functions.R")
 
 ## Use your own credentials file
 db_credentials<-"C:\\Users\\justin\\Desktop\\xmedia.cnf"
