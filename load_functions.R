d621bb8bc652edff831f22e85832b972a0b6c292
Justin Herman <justinherman42@gmail.com>
2019-08-20 14:35:50 -0400

    got rid of unused function call

--- load_functions.R
+++ load_functions.R
@@ -1,6 +1,5 @@
 ### Functions
 
-source_github("https://raw.githubusercontent.com/justinherman42/Company_project/master/load_packages.R")
 
 ### Function accesses financial data API and returns a dataframe ready to be inserted into SQL 
 ### Function takes companies stock tag as an input string  
